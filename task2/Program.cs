09a179fe4cf14b3a2ba2bf345236c583782f0683
Nick <mikabrmail@gmail.com>
2022-09-04 16:47:07 +0300

    task2 complete

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n (? т.е. значение ячейки равно сумме ее индексов?)
+
+int[,] CreateMatrixSumm(int length, int width)
+{
+    int[,] matrix = new int[length, width];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = i + j;
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrixSumm(5, 5);
+PrintMatrix(matrix);
